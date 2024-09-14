cf1d025ab83cfa17d19818d9bc53725cce7c50aa
Javieryahir <141672198+JavierYahirJuarez@users.noreply.github.com>
2024-09-14 17:46:47 -0600

    add changes

--- src/components/BotChatCard.tsx
+++ src/components/BotChatCard.tsx
@@ -1,16 +1,14 @@
 
 
+import React from 'react';
 
-const UserChatCard: React.FC<{
+const BotChatCard: React.FC<{
     message: string;
     imageUrl: string;
 }> = ({ message, imageUrl }) => {
     return (
-
-
-
-        <div className="col-start-6 col-end-13 p-3 rounded-lg">
-            <div className="flex items-center justify-start flex-row-reverse">
+        <div className="col-start-1 col-end-8 p-3 rounded-lg">
+            <div className="flex flex-row items-center">
                 <div
                     className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
                 >
@@ -21,14 +19,15 @@ const UserChatCard: React.FC<{
                     />
                 </div>
                 <div
-                    className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                    className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
                 >
-                    <div>{message}</div>
+                    <div>
+                        {message}
+                    </div>
                 </div>
             </div>
         </div>
-
     );
 }
 
-export default UserChatCard;
+export default BotChatCard;
