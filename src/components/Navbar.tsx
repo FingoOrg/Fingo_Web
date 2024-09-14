308d19a298ea124dfaaf79e6048b0db76df4f299
José Carlos Sánchez <a01174050@tec.mx>
2024-09-14 16:22:00 -0600

    changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between p-4 bg-red-600'>
+      Hola
+    </nav>
+  )
+}
+
+export default Navbar
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex-1">Home</div>
   )
 }
 
